1e6651e64135be52becf57af4952f835073536bd
Syukron Rifa'il Muttaqi <syukronrifai@gmail.com>
2017-09-21 13:48:56 +0700

    Update sample-orphan-process.c

--- sample-orphan-process.c
+++ sample-orphan-process.c
@@ -1,1 +1,19 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+int main () {
+  pid_t child_pid;
+  
+  /* Create a child process. */
+  child_pid = fork ();
+  if (child_pid > 0) {
+    /* This is the parent process. Exit immediately. */
+    exit (0);
+  } else {
+    /* This is the child process. Sleep for a minute. */
+    sleep (60);
+  }
+  
+  return 0;
+}
