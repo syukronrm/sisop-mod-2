9cb686aa8fef78882088facacd123de98470136d
Syukron Rifa'il Muttaqi <syukronrifai@gmail.com>
2017-09-22 07:45:50 +0700

    Update sample-exec.c

--- sample-exec.c
+++ sample-exec.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
 
+int main () {
+  char *argv[10] = {"mkdir", "sisop", NULL};
+  
+  execvp("mkdir", argv);
+
+  printf("This line will not be executed\n");
+
+  return 0;
+}
