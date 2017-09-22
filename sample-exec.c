8e96de5a9f6097786d6661fab200aa9b834ad17b
Syukron Rifa'il Muttaqi <syukronrifai@gmail.com>
2017-09-22 07:49:19 +0700

    Update sample-exec.c

--- sample-exec.c
+++ sample-exec.c
@@ -2,9 +2,11 @@
 #include <unistd.h>
 
 int main () {
-  char *argv[10] = {"mkdir", "sisop", NULL};
   
-  execvp("mkdir", argv);
+  // argv[n] = { {your-program-name}, {argument[1]}, {argument[2]},.....,{argument[n-2]}, NULL }
+  char *argv[4] = {"mkdir:make-directory", "-l", "/", NULL};
+  
+  execvp("ls", argv);
 
   printf("This line will not be executed\n");
 
