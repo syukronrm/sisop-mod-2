ec8ec67c7156aa542a0f8a70d54a86a4ffe339bf
Syukron Rifa'il Muttaqi <syukronrifai@gmail.com>
2017-09-24 18:16:13 +0700

    Update sample-fork-exec-wait.c

--- sample-fork-exec-wait.c
+++ sample-fork-exec-wait.c
@@ -1,1 +1,25 @@
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+int main() {
+  pid_t child_id;
+  int status;
+
+  child_id = fork();
+
+  if (child_id == 0) {
+    // this is child
+
+    char *argv[4] = {"mkdir", "-p", "sample-dir", NULL};
+    execv("/bin/mkdir", argv);
+  } else {
+    // this is parent
+
+    // the parent waits for all the child processes
+    while ((wait(&status)) > 0);
+
+    char *argv[3] = {"touch", "sample-dir/sample-touch.txt", NULL};
+    execv("/usr/bin/touch", argv);
+  }
+}
