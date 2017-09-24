14cf5c78d0f5e3e507e7d478385d65aed725a9dc
Syukron Rifa'il Muttaqi <syukronrifai@gmail.com>
2017-09-24 15:42:31 +0700

    Update sample-fork-exec.c

--- sample-fork-exec.c
+++ sample-fork-exec.c
@@ -1,1 +1,21 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+int main() {
+  pid_t child_id;
+
+  child_id = fork();
+
+  if (child_id > 0) {
+    // this is parent
+    
+    char *argv[] = {"touch", "sample-touch.txt", NULL};
+    execv("/usr/bin/touch", argv);
+  } else {
+    // this is child
+    
+    char *argv[] = {"mkdir", "sample-dir", NULL};
+    execv("/bin/mkdir", argv);
+  }
+}
