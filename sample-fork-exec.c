e0e717d69dc2c603a9d72e6f6182dab922fa9ec2
Syukron Rifa'il Muttaqi <syukronrifai@gmail.com>
2017-09-24 16:45:18 +0700

    Update sample-fork-exec.c

--- sample-fork-exec.c
+++ sample-fork-exec.c
@@ -6,16 +6,20 @@ int main() {
   pid_t child_id;
 
   child_id = fork();
+  
+  if (pid < 0) {
+    exit(EXIT_FAILURE);
+  }
 
-  if (child_id > 0) {
-    // this is parent
-    
-    char *argv[] = {"touch", "sample-touch.txt", NULL};
-    execv("/usr/bin/touch", argv);
-  } else {
+  if (child_id == 0) {
     // this is child
     
     char *argv[] = {"mkdir", "sample-dir", NULL};
     execv("/bin/mkdir", argv);
+  } else {
+    // this is parent
+  
+    char *argv[] = {"touch", "sample-touch.txt", NULL};
+    execv("/usr/bin/touch", argv);
   }
 }
